66edbe3fae77edd31f8d49ff4860c6e46497abae
Dan Serban <dserban01@yahoo.com>
2018-01-06 20:30:56 +0000

    Add content

---
+++ code/webapp.py
@@ -0,0 +1,61 @@
+# KAFKA_BROKER=localhost:9092 FLASK_PORT=9999 python webapp.py
+
+from gevent.wsgi          import WSGIServer
+from os                   import getenv
+from uuid                 import uuid1
+from json                 import dumps
+from pykafka.partitioners import hashing_partitioner
+from pykafka              import KafkaClient as KC
+from datetime             import datetime    as dt
+from flask                import ( render_template as rt
+                                 , Flask
+                                 , request
+                                 , redirect
+                                 , session )
+
+POSSIBLE_SIGNALS = \
+    [ 'aaa', 'aaaa', 'aaaaa', 'aaaaaa'
+    , 'bbb', 'bbbb', 'bbbbb', 'bbbbbb'
+    , 'ccc', 'cccc', 'ccccc', 'cccccc'
+    , 'ddd', 'dddd', 'ddddd', 'dddddd'
+    , 'eee', 'eeee', 'eeeee', 'eeeeee' ]
+
+POSSIBLE_SIGNALS_AS_SET = set(POSSIBLE_SIGNALS)
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    real_ip_addr = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    return rt( 'buttons.html'
+             , button_pressed=session.get('BUTTON_PRESSED')
+             , real_ip_addr=real_ip_addr )
+
+@app.route('/processinput', methods=['POST'])
+def processinput():
+    buttons_pressed_as_list = list( request.form )
+    if len(buttons_pressed_as_list) == 1:
+        button_pressed = buttons_pressed_as_list[0]
+        if button_pressed in POSSIBLE_SIGNALS_AS_SET:
+            real_ip_addr = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+            tstamp_str = dt.now().isoformat()
+            raw_payload = { 'ip_token': '{}_{}'.format(real_ip_addr,button_pressed)
+                          , 'tstamp': tstamp_str }
+            payload = dumps(raw_payload)
+            print(payload)
+            session['BUTTON_PRESSED'] = button_pressed
+            app.config['PRODUCER'].produce(payload, partition_key=button_pressed)
+    return redirect('/')
+
+if __name__ == '__main__':
+    kafka_broker = getenv('KAFKA_BROKER', 'localhost:9092')
+    kafka_client = KC(hosts=kafka_broker)
+    test_topic = kafka_client.topics[b'raw']
+    app.config['PRODUCER'] = \
+        test_topic.get_sync_producer(partitioner=hashing_partitioner)
+    app.config['SECRET_KEY'] = getenv( 'SECRET_SESSION_KEY'
+                                     , str( uuid1() ) )
+    flask_port = int( getenv('FLASK_PORT') )
+    http_server = WSGIServer( ('', flask_port), app )
+    http_server.serve_forever()
+
---
+++ install.sh
@@ -0,0 +1,25 @@
+export DEBIAN_FRONTEND=noninteractive
+echo 'Running apt-get -qq update ...'
+apt-get -qq update
+echo 'Running apt-get -qq install ...'
+apt-get -qq install --no-install-recommends -y \
+  wget curl git vim tmux jq mc net-tools less  \
+  ca-certificates build-essential locales      \
+  librdkafka-dev libev-dev libsnappy-dev zlib1g-dev netcat-traditional
+echo 'Downloading Anaconda ...'
+# wget -qO /opt/Anaconda.sh \
+#          https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh
+wget -qO /opt/Anaconda.sh http://192.168.0.13/AnacondaForPython3.sh
+echo 'Installing Anaconda ...'
+cd /opt
+bash Anaconda.sh -b -p /opt/anaconda
+rm /opt/Anaconda.sh
+mv /opt/anaconda/bin/sqlite3 /opt/anaconda/bin/sqlite3.orig
+echo 'Installing pykafka ...'
+# PATH=/opt/anaconda/bin:$PATH pip install pykafka
+pip install pykafka
+echo 'Getting code from GitHub ...'
+cd /opt
+git clone https://github.com/dserban/ClickStreamToKafka
+echo 'Building container, this may take a while ...'
+
