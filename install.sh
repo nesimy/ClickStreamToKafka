8c8affda418d172dd2abb74d376a651919762933
Dan Serban <dserban01@yahoo.com>
2018-01-15 18:35:00 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,21 @@ echo 'Running apt-get -qq install ...'
 apt-get -qq install --no-install-recommends -y \
   wget curl git vim tmux jq mc net-tools less  \
   ca-certificates build-essential locales      \
-  librdkafka-dev libev-dev libsnappy-dev zlib1g-dev netcat-traditional
+  libev-dev libsnappy-dev zlib1g-dev netcat-traditional
+echo 'Downloading librdkafka ...'
+wget -qO /opt/librdkafka.tgz \
+         https://github.com/edenhill/librdkafka/archive/v0.11.3.tar.gz
+echo 'Extracting librdkafka ...'
+tar -xf /opt/librdkafka.tgz -C /opt
+rm /opt/librdkafka.tgz
+mv /opt/librdkafka* /opt/librdkafka
+echo 'Building librdkafka ...'
+cd /opt/librdkafka
+./configure
+make
+make install
+cd /opt
+rm -rf /opt/librdkafka
 echo 'Downloading Anaconda ...'
 wget -qO /opt/Anaconda.sh \
          https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh
@@ -14,10 +28,9 @@ cd /opt
 bash Anaconda.sh -b -p /opt/anaconda
 rm /opt/Anaconda.sh
 mv /opt/anaconda/bin/sqlite3 /opt/anaconda/bin/sqlite3.orig
-echo 'Installing pykafka ...'
-pip install pykafka
+echo 'Installing confluent-kafka ...'
+pip install confluent-kafka
 echo 'Getting code from GitHub ...'
 cd /opt
 git clone https://github.com/dserban/ClickStreamToKafka
 echo 'Building container, this may take a while ...'
-
